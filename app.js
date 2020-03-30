fd3659e547b1f3e0f6d6c7b9aa4e763fc63739c1
vasudha-posten <58505858+vasudha-posten@users.noreply.github.com>
2020-03-30 07:18:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,33 @@ var activity    = require('./routes/activity');
 
 var app = express();
 
+var APIKeys = {
+    appId           : '40eb91d1-b18a-4648-b260-d37d87b9a2fb',
+    clientId        : '16rne6pbteqrl980mzbckh0e',
+    clientSecret    : 'CuKfVcy0A1ILbFAyHcEDLZC6',
+    appSignature    : 'n4C6s0pEF4lA1ePrhFRHRXRfNBO_INV9S9Cnl_Jnpl5-E8B7C6f8wre7Wx5PJk5oCOsLWhQgW2dJ46qNtKcvWV_HSqusOxNWOkyYiYUSWv3tzxs39oZp0LgCCEE5dX99H2SYNh5fREIVtusZEHg8rwvK88qysk5wJOAkleaVJsRCV1FDRXSRNjqNPJa5TDw5Cqh0b1s_cbrICeHf1oXsy18H8KJQ4adDlnBjEGQXnrwcKRLMQIztMz_ePLUsJQ2',
+    authUrl         : 'https://auth.exacttargetapis.com/v1/requestToken?legacy=1'
+};
+
+
+// Simple custom middleware
+function tokenFromJWT( req, res, next ) {
+    // Setup the signature for decoding the JWT
+    var jwt = new JWT({appSignature: APIKeys.appSignature});
+    
+    // Object representing the data in the JWT
+    var jwtData = jwt.decode( req );
+
+    // Bolt the data we need to make this call onto the session.
+    // Since the UI for this app is only used as a management console,
+    // we can get away with this. Otherwise, you should use a
+    // persistent storage system and manage tokens properly with
+    // node-fuel
+    req.session.token = jwtData.token;
+    next();
+}
+
+
 // Configure Express
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.raw({type: 'application/jwt'}));
