46fbd1af37692a2bdc707c0e244112151964c87a
vasudha-posten <58505858+vasudha-posten@users.noreply.github.com>
2020-04-01 23:49:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,3 +41,18 @@ app.post('/journeybuilder/execute/', activity.execute );
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+// HubExchange Routes
+app.get('/', routes.index );
+app.post('/login', routes.login );
+app.post('/logout', routes.logout );
+
+// Custom Hello World Activity Routes
+app.post('/journeybuilder/save/', activity.save );
+app.post('/journeybuilder/validate/', activity.validate );
+app.post('/journeybuilder/publish/', activity.publish );
+app.post('/journeybuilder/execute/', activity.execute );
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
